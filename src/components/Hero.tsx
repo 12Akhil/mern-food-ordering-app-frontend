e287de9c19405192a7bac6d0863d6144de0d2fd8
12Akhil <akhilkumarsingh2003@gmail.com>
2024-06-11 16:15:25 +0530

    adding res

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import hero from "../assets/hero.png";
 const Hero=()=>{
     return(
         <div>
-            <img src={hero} className="w-full max-h-[600px] object-cover" />
+            <img src={hero} className="w-full max-h-[500px] object-cover" />
         </div>
     );
 }
