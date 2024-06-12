c6577a422815bdf729fe151d9b800b33cc876daf
12Akhil <akhilkumarsingh2003@gmail.com>
2024-06-12 17:52:35 +0530

    added more

---
+++ src/components/MenuItem.tsx
@@ -0,0 +1,22 @@
+import { MenuItem as Menu } from "../types";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+
+type Props = {
+  menuItem: Menu;
+  addToCart: () => void;
+};
+
+const MenuItem = ({ menuItem, addToCart }: Props) => {
+  return (
+    <Card className="cursor-pointer" onClick={addToCart}>
+      <CardHeader>
+        <CardTitle>{menuItem.name}</CardTitle>
+      </CardHeader>
+      <CardContent className="font-bold">
+      ₹{(menuItem.price)}
+      </CardContent>
+    </Card>
+  );
+};
+
+export default MenuItem;
